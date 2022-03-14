c290e665497403ea31d7a914c8a821e0f77853c2
Denver McCarthy <dmichaelspdx@gmail.com>
2022-03-14 13:43:42 -0700

    tests passing

--- src/components/BlogCard/BlogCard.test.js
+++ src/components/BlogCard/BlogCard.test.js
@@ -5,6 +5,7 @@ test('should render the blog card with title, subtitle, author and text', () => 
   const container = render(
     <BlogCard
       title="Test title"
+      authors="test"
       subtitle="Test subtitle"
       text="lorem ipsum blah blah blah"
       image="https://www.fillmurray.com/300/200"
@@ -12,4 +13,5 @@ test('should render the blog card with title, subtitle, author and text', () => 
   );
   const title = screen.getByText('Test title');
   expect(title).toBeVisible();
+  expect(container).toMatchSnapshot();
 });
